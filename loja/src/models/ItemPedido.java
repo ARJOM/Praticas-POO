298e4bd26d8c9e031242883d82b6ec51a93bd29a
Ricart <antonio.ricart@academico.ifpb.edu.br>
2019-10-17 08:59:34 -0300

    adicionando classe itempedido

---
+++ loja/src/models/ItemPedido.java
@@ -0,0 +1,38 @@
+package models;
+
+public class ItemPedido {
+
+    private Produto produto;
+    private int quantidade;
+    private float precoUnitario;
+
+    public ItemPedido(Produto produto, int quantidade) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+        this.precoUnitario = produto.getValor();
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public float getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public void setPrecoUnitario(float precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+}
