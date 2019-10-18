8de310fee672845ad98abac0d3fb30086e64b057
Ricart <antonio.ricart@academico.ifpb.edu.br>
2019-10-18 06:26:44 -0300

    iniciando classe loja

--- loja/src/models/Loja.java
+++ loja/src/models/Loja.java
@@ -1,7 +1,31 @@
 package models;
 
+import java.lang.ref.Cleaner;
+
 public class Loja {
 
-    private String nome;
+    private Produto[] produtos;
+    private Cliente[] clientes;
 
+    public Loja() {
+        this.produtos = new Produto[10];
+        this.clientes = new Cliente[10];
+    }
+
+    public Produto[] getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(Produto[] produtos) {
+        this.produtos = produtos;
+    }
+
+    public Cliente[] getClientes() {
+        return clientes;
+    }
+
+    public void setClientes(Cliente[] clientes) {
+        this.clientes = clientes;
+    }
+    
 }
